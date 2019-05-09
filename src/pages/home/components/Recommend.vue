ed44297b9ac62f9f9b411ee91fbbb97e65e05123
Jhong <804381249@qq.com>
2019-05-09 10:14:39 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,83 @@
+<template>
+	<div>
+		<div class="title">热销推荐</div>
+		<ul>
+			<li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+
+				<img class="item-img" :src="item.imgUrl"/>
+
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+					<button class="item-button">查看详情</button>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'HomeRecommend',
+		data () {
+			return {
+				recommendList: [{
+					id: '0001',
+					imgUrl: 'http://img1.qunarzz.com/sight/p0/1902/7a/7ac34ad6cc6e7497a3.img.jpg_200x200_939e55b1.jpg',
+					title: '东莞龙凤山庄影视渡假村',
+					desc: '与明星的邂逅'
+				},{
+					id: '0002',
+					imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/34/34170b200db08a5a90.img.jpg_200x200_cc3130fd.jpg',
+					title: '隐贤山庄',
+					desc: '与明星的邂逅'
+				},{
+					id: '0003',
+					imgUrl: 'http://img1.qunarzz.com/sight/p0/1411/84/00648f910d93824808f2781486ca5cb8.water.jpg_200x200_9c0de28d.jpg',
+					title: '水立方游泳馆',
+					desc: '与明星的邂逅'
+				}]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped="scoped">
+	@import '~styles/mixins.styl'
+	.title
+		margin-top: .2rem
+		line-height: .8rem
+		background: #eee
+		text-indent: .2rem
+	.item
+		overflow: hidden
+		display: flex
+		height: 1.9rem
+	.border-bottom
+		border-bottom: .01rem solid #eee
+		.item-img
+			width: 1.7rem
+			height: 1.7rem
+			padding: .1rem			
+		.item-info
+			flex: 1
+			padding: .1rem
+			min-width: 0
+			.item-title
+				line-height: .54rem
+				font-size: .32rem
+				ellipsis()
+			.item-desc
+				line-height: .54rem
+				color: #ccc
+				ellipsis()
+			.item-button
+				line-height: .44rem
+				margin-top: .16rem
+				background: #ff9300
+				padding: 0 .2rem
+				border-radius: 0.6rem
+				color: #fff
+				ellipsis()
+		
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,74 @@
+<template>
+	<div>
+		<div class="title">周末去哪儿</div>
+		<ul>
+			<li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+
+				<div class="item-img-wrapper">
+					<img class="item-img" :src="item.imgUrl"/>
+				</div>
+
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+					
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'HomeWeekend',
+		data () {
+			return {
+				recommendList: [{
+					id: '0001',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+					title: '东莞龙凤山庄影视渡假村',
+					desc: '与明星的邂逅'
+				},{
+					id: '0002',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1811/b8/5d599bbdcf8b57.jpg_r_640x214_2ee055e3.jpg',
+					title: '东莞龙凤山庄影视渡假村',
+					desc: '与明星的邂逅'
+				},{
+					id: '0003',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4b/d04aa63b1b0ab3.jpg_r_640x214_a3a9c5b6.jpg',
+					title: '东莞龙凤山庄影视渡假村',
+					desc: '与明星的邂逅'
+				}]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped="scoped">
+	@import '~styles/mixins.styl'
+	.title
+		margin-top: .2rem
+		line-height: .8rem
+		background: #eee
+		text-indent: .2rem
+	.item-img-wrapper
+		overflow: hidden
+		height: 0
+		padding-bottom: 33.9%
+	.border-bottom
+		border-bottom: .01rem solid #eee
+		.item-img
+			widht: 100%		
+		.item-info			
+			padding: .1rem
+			.item-title
+				line-height: .54rem
+				font-size: .32rem
+				ellipsis()
+			.item-desc
+				line-height: .54rem
+				color: #ccc
+				ellipsis()
+			
+		
+</style>
