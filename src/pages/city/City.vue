e7fc155a9f40d3cc3073207238b1d275923c8ba2
Jhong <804381249@qq.com>
2019-05-11 12:12:52 +0800

    perfomance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,15 @@
 	<div>
 		<city-header></city-header>
 		<city-search></city-search>
-		<city-list :cities="cities" :hot="hotCities"></city-list>
-		<city-alphabet :cities="cities"></city-alphabet>
+		<city-list 
+			:cities="cities" 
+			:hot="hotCities"
+			:letter="letter"
+			></city-list>
+		<city-alphabet 
+			:cities="cities"
+			@change="handleLetterChange"
+		></city-alphabet>
 	</div>
 </template>
 
@@ -28,7 +35,8 @@ import CityAlphabet from './components/Alphabet'
 		data () {
 			return{
 				cities: {},
-				hotCities: []
+				hotCities: [],
+				letter: ''
 			}
 		},
 		methods:{
@@ -42,6 +50,9 @@ import CityAlphabet from './components/Alphabet'
 					this.cities = data.cities
 					this.hotCities = data.hotCities
 				}
+			},
+			handleLetterChange(letter){
+				this.letter = letter
 			}
 		},
 		mounted(){
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -19,7 +19,12 @@
 					
 				</div>
 			</div>
-			<div class="area" v-for="(item,key) of cities" :key="key">
+			<div 
+				class="area" 
+				v-for="(item,key) of cities" 
+				:key="key"
+				:ref="key"
+				>
 				<div class="title border-topbottom">{{key}}</div>
 				<div class="item-list">
 					<div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
@@ -40,10 +45,19 @@
 		name: 'CityList',
 		props:{
 			hot: Array,
-			cities: Object
+			cities: Object,
+			letter: String
 		},
 		mounted(){
 			this.scroll = new Bscroll(this.$refs.wrapper)
+		},
+		watch: {
+			letter () {
+				if (this.letter){
+					const element = this.$refs[this.letter][0]
+					this.scroll.scrollToElement(element)
+				}
+			}
 		}
 	}
 </script>
