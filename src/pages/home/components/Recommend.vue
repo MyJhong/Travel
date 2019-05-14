9ac63db01c83143cc2f3bb88481217b676b69988
Jhong <804381249@qq.com>
2019-05-14 12:56:55 +0800

    banner finish

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,16 +2,17 @@
 	<div>
 		<div class="title">热销推荐</div>
 		<ul>
-			<li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+				<router-link tag="li"  class="item border-bottom" v-for="item of recommendList" :key="item.id" :to="'/detail/'+item.id">
+	
+					<img class="item-img" :src="item.imgUrl"/>
+	
+					<div class="item-info">
+						<p class="item-title">{{item.title}}</p>
+						<p class="item-desc">{{item.desc}}</p>
+						<button class="item-button">查看详情</button>
+					</div>
+				</router-link>
 
-				<img class="item-img" :src="item.imgUrl"/>
-
-				<div class="item-info">
-					<p class="item-title">{{item.title}}</p>
-					<p class="item-desc">{{item.desc}}</p>
-					<button class="item-button">查看详情</button>
-				</div>
-			</li>
 		</ul>
 	</div>
 </template>
